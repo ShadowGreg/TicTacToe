62450ee3811719eaed7c997bc620dfa05fe3e7ac
ShadowGreg <shadow-greg@yandex.ru>
2022-10-17 16:24:24 +0300

    add UnitTests

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using TicTacToe;
+
+
+int size = 3;
+var playField = new PlayField(size);
+Motion FirstPlayer = new Motion(playField);
+Motion SecondPlayer = new Motion(playField);
+int row = 1;
+int column = 1;
+
+FirstPlayer.SetMotion(row, column, PlayerIcon.O);
+SecondPlayer = FirstPlayer.GetComputerMotion();
