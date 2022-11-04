ab0b027440b0ef3533a2368922d76ff874660739
ShadowGreg <shadow-greg@yandex.ru>
2022-11-04 09:42:10 +0300

    new wfp

--- UnitTests/MotionTests.cs
+++ UnitTests/MotionTests.cs
@@ -36,6 +36,26 @@ public class MotionTests
             Assert.That(motionItem2, Is.EqualTo(true));
         });
     }
+    [Test]
+    public void Tow_Players_Step_Motions_Test_4x4()
+    {
+        int playFieldSize = 4;
+        var playerIcon1 = PlayerIcon.O;
+        var playerIcon2 = PlayerIcon.X;
+        var playField = new PlayField(playFieldSize);
+        var testMotion = new Motion(playerIcon1);
+        int testColumn1 = 2;
+        int testRow1 = 2;
+        int testColumn2 = 1;
+        int testRow2 = 1;
+        bool motionItem1 = testMotion.StepMotions(testRow1, testColumn1, playField);
+        bool motionItem2 = testMotion.StepMotions(testRow2, testColumn2, playField);
+        Assert.Multiple(() =>
+        {
+            Assert.That(motionItem1, Is.EqualTo(true));
+            Assert.That(motionItem2, Is.EqualTo(true));
+        });
+    }
 
     [Test]
     public void Impossible_Step_Motions_Test()
